b0c337646ee23aaed67ecfeda5c0d3ca847eb3ff
Leo Guinan <leo.guinan@gmail.com>
2024-05-21 13:18:33 -0400

    rounding logo in chat

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -30,7 +30,8 @@ export function ChatList({messages, user, chatMessageLoading}: ChatList) {
                                 className='flex size-8 shrink-0 select-none items-center justify-center rounded-full bg-primary text-primary-foreground'
 
                             >
-                                <Image src="/logo.png" width={32} height={32} alt="Score My Deck Logo"/>
+                                <Image src="/logo.png" width={32} height={32} alt="Score My Deck Logo"
+                                       className="rounded-full"/>
 
 
                             </div>
@@ -63,7 +64,7 @@ export function ChatList({messages, user, chatMessageLoading}: ChatList) {
                             className='flex size-8 shrink-0 select-none items-center justify-center rounded-full bg-primary text-primary-foreground'
 
                         >
-                            <Image src="/logo.png" width={32} height={32} alt="Score My Deck Logo"/>
+                            <Image src="/logo.png" width={32} height={32} alt="Score My Deck Logo" className="rounded-full"/>
 
 
                         </div>
